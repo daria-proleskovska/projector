e82b7f88baaaa5132e5f499ca284de0c9ead4623
Daria Proleskovska <dproleskovska@gmail.com>
2023-11-27 02:55:16 +0200

    Sixth homework
    - fix delete functionality
    - add edit functionality
    - add styles

--- hw6/index.js
+++ hw6/index.js
@@ -4,25 +4,30 @@ const taskInput = document.querySelector('.task-input');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const form = document.querySelector('.create-task-form');
+const editForm = document.querySelector('.edit-task-form');
+const editTaskInput = document.querySelector('.edit-task-input');
+const editTaskCancelBtn = document.querySelector('.edit-task-cancel');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
+    
+    closeEditForm();
 
-    if (taskInput.value.trim() === '') {
+    const inputValue = taskInput.value.trim();
+
+    if (inputValue === '') {
         return;
     }
 
-    createSingleTaskElement(taskInput.value);
+    createSingleTaskElement(inputValue);
 
-    storeTaskInLocalStorage(taskInput.value);
+    storeTaskInLocalStorage(inputValue);
 
     taskInput.value = '';
 })
 
 document.addEventListener('DOMContentLoaded', () => {
-    const tasks = localStorage.getItem('tasks') !== null
-        ? JSON.parse(localStorage.getItem('tasks'))
-        : [];
+    const tasks = getStoredTasksArray();
 
     tasks.forEach((task) => {
         createSingleTaskElement(task);
@@ -30,28 +35,54 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 taskList.addEventListener('click', (event) => {
-    const iconContainer = event.target.parentElement;
+    const iconContainer = event.target;
 
-    if (iconContainer.classList.contains('delete-item')) {
-        if (confirm('Are you sure?')) {
-            iconContainer.parentElement.remove();
-            const tasks = localStorage.getItem('tasks') !== null
-                ? JSON.parse(localStorage.getItem('tasks'))
-                : [];
+    if (iconContainer.closest('.delete-item') !== null) {
+        const currentLi = iconContainer.closest('li');
 
-            tasks.forEach((task, index) => {
-                if (iconContainer.parentElement.textContent === task) {
-                    tasks.splice(index, 1);
-                }
-            })
+        if (confirm(`Ви дійсно хочете видалити "${currentLi.firstChild.nodeValue}"?`)) {
+            const currentTaskIndex = retrieveTaskIndex(currentLi);
+            const tasks = getStoredTasksArray();
+            
+            currentLi.remove();
 
+            tasks.splice(currentTaskIndex, 1);
             localStorage.setItem('tasks', JSON.stringify(tasks));
         }
+    } else if (iconContainer.closest('.edit-item') !== null) {
+        const currentLi = iconContainer.closest('li');
+        
+        currentLi.append(editForm);
+        editForm.hidden = false;
+        editTaskInput.value = currentLi.firstChild.nodeValue;
+        editTaskInput.focus();
     }
 })
 
+editForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const inputValue = editTaskInput.value.trim();
+
+    if (inputValue === '') {
+        return;
+    }
+
+    const currentLi = editForm.parentElement;
+    const currentTaskIndex = retrieveTaskIndex(currentLi);
+    const tasks = getStoredTasksArray();
+
+    currentLi.firstChild.nodeValue = inputValue;
+    closeEditForm();
+    
+    tasks.splice(currentTaskIndex, 1, inputValue);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+})
+
+editTaskCancelBtn.addEventListener('click', closeEditForm);
+
 clearBtn.addEventListener('click', () => {
-    if (confirm('Are you sure?')) {
+    if (confirm('Ви дійсно хочете видалити всі завдання?')) {
         localStorage.clear();
         taskList.innerHTML = '';
     }
@@ -62,20 +93,44 @@ function createSingleTaskElement(newTask) {
     li.className = 'collection-item';
     li.appendChild(document.createTextNode(newTask));
 
+    const editElement = document.createElement('span');
+    editElement.className = 'edit-item';
+    editElement.innerHTML = '<i class="fa fa-edit"></i>';
+    
     const deleteElement = document.createElement('span');
     deleteElement.className = 'delete-item';
     deleteElement.innerHTML = '<i class="fa fa-remove"></i>';
 
-    li.appendChild(deleteElement);
+    li.append(editElement, deleteElement);
 
     taskList.appendChild(li);
 }
 
 function storeTaskInLocalStorage(newTask) {
-    const tasks = localStorage.getItem('tasks') !== null
-        ? JSON.parse(localStorage.getItem('tasks'))
-        : [];
+    const tasks = getStoredTasksArray();
 
     tasks.push(newTask);
     localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function getStoredTasksArray() {
+    return localStorage.getItem('tasks') !== null
+        ? JSON.parse(localStorage.getItem('tasks'))
+        : [];
+}
+
+function retrieveTaskIndex(li) {
+    const taskListArray = Array.from(taskList.children);
+
+    for (let i = 0; i < taskListArray.length; i++) {
+        if (li === taskListArray[i]) {
+            return i;
+        }
+    }
+}
+
+function closeEditForm() {
+    taskList.after(editForm);
+    editTaskInput.value = '';
+    editForm.hidden = true;
 }
