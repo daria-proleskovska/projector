c747efd2901cd558bbc5794fa4fcb6ac70aad54d
Daria Proleskovska <dproleskovska@gmail.com>
2023-12-13 23:58:39 +0200

    JS9:  Weather app
    Initial commit

---
+++ js9/constants.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('.form');
+const input = document.querySelector('#input');
+const list = document.querySelector('.cities');
+const msg = document.querySelector('.msg');
+const API_KEY = '8e844b0959d689f821164703a8fa367c';
+
+export {form, input, list, msg, API_KEY};
