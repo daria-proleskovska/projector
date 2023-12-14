fe7a577c70713ad380543f1c33828012d9f410d5
Daria Proleskovska <dproleskovska@gmail.com>
2023-12-14 14:26:07 +0200

    JS9: Weather app
    - move <script> to the <header>
    - add "readyState" checking

--- js9/index.js
+++ js9/index.js
@@ -2,13 +2,25 @@ import {WeatherApp} from './weatherApp.js';
 import {API_KEY, form, input} from './constants.js'
 
 (() => {
-    const weatherApp = new WeatherApp(API_KEY);
+    const readyState = document.readyState;
+    
+    if (readyState === 'interactive' || readyState === "complete") {
+        initWeatherApp();
+    } else {
+        window.addEventListener('DOMContentLoaded', () => {
+            initWeatherApp();
+        });
+    }
 
-    form.addEventListener('submit', async (event) => {
-        event.preventDefault();
-        await weatherApp.fetchData();
+    function initWeatherApp() {
+        const weatherApp = new WeatherApp(API_KEY);
 
-        form.reset();
-        input.focus();
-    })
+        form.addEventListener('submit', async (event) => {
+            event.preventDefault();
+            await weatherApp.fetchData();
+
+            form.reset();
+            input.focus();
+        })
+    }
 })()
